9d7585da3ed5af99c1d526ae37d2621a57d22ac9
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-11 03:23:17 +0000

    add a Header component to dashboard layout

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <div className="md:hidden mb-3">
+          Hamburger Btn goes here on small screens
+        </div>
+      </nav>
+      <div>header content goes here</div>
+    </header>
+  );
+};
+
+export default NavBar;
