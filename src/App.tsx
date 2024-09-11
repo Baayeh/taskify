c1d6feb74314ef795e47e97f5eb66608a1e263ef
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-11 03:19:42 +0000

    create root and dashboard layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
