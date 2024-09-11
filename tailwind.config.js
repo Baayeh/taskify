0e67830c98518416dffedfd67c46ebeb556d191a
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-11 02:55:10 +0000

    add custom colors to tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4CAF50",
+        secondary: "#FF4081",
+        darkBackground: "#1E1E1E",
+        darkCardBackground: "#252526",
+        darkText: "#FFFFFF",
+        darkSecondaryText: "#A6A6A6",
+        darkBorder: "#404040",
+      },
+    },
+    screens: {
+      md: "820px",
+    },
   },
   plugins: [],
 };
