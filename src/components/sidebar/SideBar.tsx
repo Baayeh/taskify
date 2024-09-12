61a6e19be7d6d4a7e9b04b4fe22c6526a8638772
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-12 08:27:57 +0000

    add a dropdown menu to manage user accounts

---
+++ src/components/sidebar/SideBar.tsx
@@ -0,0 +1,57 @@
+import profile from "../../assets/creed.webp";
+import { ChevronsUpDown, Settings, UserCog } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+const SideBar = () => {
+  return (
+    <nav
+      id="sidebar"
+      className="fixed hidden md:block bg-muted w-[20rem] h-screen p-2
+      "
+    >
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="w-full justify-start gap-x-3 h-20 focus-visible:ring-0"
+          >
+            <div className="text-4xl font-bold rounded-full bg-background border">
+              <img
+                src={profile}
+                alt="AC"
+                className="rounded-full w-12 h-12 object-cover"
+              />
+            </div>
+            <div className="text-start">
+              <h2 className="text-lg font-semibold">Adonis Creed</h2>
+              <p className="text-sm text-gray-300 flex items-center">
+                <span>adonis.creed@gmail.com</span>
+                <span>
+                  <ChevronsUpDown size={14} />
+                </span>
+              </p>
+            </div>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-[18rem] p-0 bg-muted border border-muted-foreground/30">
+          <DropdownMenuItem className="p-4 flex items-center gap-x-5 text-muted-foreground hover:!bg-muted-foreground/10 hover:text-foreground border-b border-muted-foreground/30">
+            <UserCog />
+            <span className="text-base">Manage accounts</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="p-4 flex items-center gap-x-5 text-muted-foreground hover:!bg-muted-foreground/10 hover:text-foreground border-b border-muted-foreground/30">
+            <Settings />
+            <span className="text-base">Settings</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
+  );
+};
+
+export default SideBar;
