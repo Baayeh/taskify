d1b9924de87bd18e7b49e0641f5c27cb20258599
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-12 19:36:17 +0000

    make minor changes on the sidebar

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -14,24 +14,23 @@ import {
 
 const SideBar = () => {
   return (
-    <>
+    <div className="md:border-r">
       <AccountSettings />
 
-      <div className="mt-1">
+      <div className="mt-1 md:px-4">
         <Label htmlFor="search" className="relative">
           <Input
             id="search"
             placeholder="Search..."
-            className="border-muted-foreground/30 border-b-2 border-b-foreground/50 focus-visible:ring-0 focus:border-b-primary"
+            className="pr-7 border-muted-foreground/30 border-b-2 border-b-foreground/50 focus-visible:ring-0 focus:border-b-primary"
           />
-          <Search
-            size={16}
-            className="absolute right-2 top-1/2 -translate-y-1/2 text-muted-foreground"
-          />
+          <div className="absolute right-2 top-1/2 -translate-y-1/2 text-muted-foreground">
+            <Search size={16} />
+          </div>
         </Label>
       </div>
 
-      <ScrollArea className="relative w-full h-[calc(100vh-116px)] px-4 py-2">
+      <ScrollArea className="relative w-full h-[calc(100vh-116px)] md:px-4 py-2">
         <MenuList />
 
         <div className="absolute left-0 bottom-10 flex items-center justify-between gap-x-3 w-full">
@@ -57,7 +56,7 @@ const SideBar = () => {
           </TooltipProvider>
         </div>
       </ScrollArea>
-    </>
+    </div>
   );
 };
 
