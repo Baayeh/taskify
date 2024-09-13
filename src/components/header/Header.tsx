24056fb84b1efb571c0da3689f054a9d0aff43ad
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-13 06:15:43 +0000

    update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,28 +11,32 @@ const Header: React.FC<HeaderProps> = ({ setOpen }) => {
 
   return (
     <header>
-      <div>
-        <div className="md:hidden mb-3">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="hover:bg-muted"
-            onClick={() => setOpen(true)}
-          >
-            <Menu className="text-primary" />
-          </Button>
-        </div>
-        <section>
-          <div>
-            <h1
-              className={`text-3xl font-semibold flex items-center gap-x-5 ${color}`}
-            >
-              {icon}
-              <span>{title}</span>
-            </h1>
-          </div>
-        </section>
+      <div className="md:hidden mb-3">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="hover:bg-muted"
+          onClick={() => setOpen(true)}
+        >
+          <Menu className="text-primary" />
+        </Button>
       </div>
+      <section className="flex items-center justify-between">
+        <div>
+          <h1
+            className={`text-3xl font-semibold flex items-center gap-x-5 ${color}`}
+          >
+            {icon}
+            <span>{title}</span>
+          </h1>
+        </div>
+
+        <div>
+          {
+            // TODO: Add button to change theme and do other stuff
+          }
+        </div>
+      </section>
     </header>
   );
 };
