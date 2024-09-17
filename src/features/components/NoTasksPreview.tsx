50a9c67c9331162c4a97d49c83965a4a21e7b53f
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-17 07:03:28 +0000

    Update component to fit on small screens

--- src/features/components/NoTasksPreview.tsx
+++ src/features/components/NoTasksPreview.tsx
@@ -1,14 +1,17 @@
 import emptySVG from "@/assets/empty.svg";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const NoTasksPreview = () => {
   return (
-    <div className="lg:w-1/2 lg:mx-auto mt-[10rem] md:mt-[22rem] lg:mt-[10rem]">
-      <img src={emptySVG} alt="empty" className="w-1/3 mx-auto" />
-      <p className="mt-5 text-sm text-primary text-center px-[3rem] sm:w-1/2 sm:px-0 md:w-[65%] lg:w-1/2 mx-auto">
-        Tasks show up here if they aren&apos;t part of any lists you&apos;ve
-        created.
-      </p>
-    </div>
+    <ScrollArea className="relative w-full h-[calc(100vh-15rem)] md:h-[calc(100vh-12rem)]">
+      <div className="lg:w-1/2 lg:mx-auto mt-[7rem] md:mt-[22rem] lg:mt-[10rem]">
+        <img src={emptySVG} alt="empty" className="w-1/3 mx-auto" />
+        <p className="mt-5 text-sm text-primary text-center px-[3rem] sm:w-1/2 sm:px-0 md:w-[65%] mx-auto">
+          Tasks show up here if they aren&apos;t part of any lists you&apos;ve
+          created.
+        </p>
+      </div>
+    </ScrollArea>
   );
 };
 
