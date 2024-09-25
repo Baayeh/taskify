7ca1209100a936e3a707ff7eef8d5dfd91e19288
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-25 08:37:12 +0000

    add more endpoints to the tasks services

--- src/features/redux/slices/taskSlice.ts
+++ src/features/redux/slices/taskSlice.ts
@@ -1,29 +1,68 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { Task } from "@/types/tasks";
 import { RootState } from "../store";
 
 interface TaskState {
+  task: Task;
   tasks: Task[];
+  importantTasks: Task[];
+  completedTasks: Task[];
+  plannedTasks: Task[];
 }
 
 const initialState: TaskState = {
+  task: {} as Task,
   tasks: [],
+  importantTasks: [],
+  completedTasks: [],
+  plannedTasks: [],
 };
 
 export const taskSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
+    setTask: (state, action: PayloadAction<Task>) => {
+      state.task = action.payload;
+    },
     setTasks: (state, action: PayloadAction<Task[]>) => {
       state.tasks = action.payload;
     },
     addTask: (state, action: PayloadAction<Task>) => {
       state.tasks.push(action.payload);
     },
+    setImportantTasks: (state, action: PayloadAction<Task[]>) => {
+      state.importantTasks = action.payload;
+    },
+    setCompletedTasks: (state, action: PayloadAction<Task[]>) => {
+      state.completedTasks = action.payload;
+    },
+    setPlannedTasks: (state, action: PayloadAction<Task[]>) => {
+      state.plannedTasks = action.payload;
+    },
   },
 });
 
-export const { setTasks, addTask } = taskSlice.actions;
+export const {
+  setTasks,
+  addTask,
+  setImportantTasks,
+  setCompletedTasks,
+  setPlannedTasks,
+} = taskSlice.actions;
 export const selectTasks = (state: RootState) => state.tasks;
+
+export const selectImportantTasksCount = createSelector(
+  (state: RootState) => state.tasks.tasks,
+  (tasks) => tasks.filter((task) => task.important).length
+);
+
+export const selectPlannedTasksCount = createSelector(
+  (state: RootState) => state.tasks.tasks,
+  (tasks) =>
+    tasks.filter((task) => task.due_date != null || task.reminder != null)
+      .length
+);
+
 export default taskSlice.reducer;
--- src/features/services/tasks.ts
+++ src/features/services/tasks.ts
@@ -6,3 +6,21 @@ export const GET_ALL_TASKS = async (): Promise<Task[]> => {
   const res = await api.get<Task[]>("/tasks/");
   return res.data;
 };
+
+// fetch single task
+export const GET_TASK = async (id: number): Promise<Task> => {
+  const res = await api.get<Task>(`/tasks/${id}/`);
+  return res.data;
+};
+
+// fetch important tasks
+export const GET_IMPORTANT_TASKS = async (): Promise<Task[]> => {
+  const res = await api.get<Task[]>("/tasks/important/");
+  return res.data;
+};
+
+// fetch planned tasks
+export const GET_PLANNED_TASKS = async (): Promise<Task[]> => {
+  const res = await api.get<Task[]>("/tasks/planned/");
+  return res.data;
+};
