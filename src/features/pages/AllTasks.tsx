ad6fae2fe340b61096c7d0d2d870316c6abd39d6
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-25 04:11:09 +0000

    fetch and display all tasks from API

--- src/features/pages/AllTasks.tsx
+++ src/features/pages/AllTasks.tsx
@@ -1,19 +1,43 @@
-import { Task } from "@/types/tasks";
 import NoTasksPreview from "../components/NoTasksPreview";
 
 import TasksList from "./TaskList";
+import { useAppDispatch, useAppSelector } from "@/lib/utils";
+import { selectTasks, setTasks } from "../redux/slices/taskSlice";
+import { GET_ALL_TASKS } from "../services/tasks";
+import { useLoader } from "@/hooks/useLoader";
+import { useEffect } from "react";
 
 const AllTasks = () => {
-  const tasks: Task[] = [];
+  const { showLoader } = useLoader();
+  const { tasks } = useAppSelector(selectTasks);
+  const dispatch = useAppDispatch();
+
+  // get all tasks from services
+  const getAllTasks = async () => {
+    showLoader(true);
+
+    try {
+      const data = await GET_ALL_TASKS();
+
+      if (data) {
+        dispatch(setTasks(data));
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    } finally {
+      showLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    void getAllTasks();
+  }, []);
 
   return (
     <>
       <section className="w-full">
         {tasks.length === 0 && <NoTasksPreview />}
 
-        {
-          // TODO: Add a task card for each task
-        }
         {tasks.length ? <TasksList tasks={tasks} /> : null}
       </section>
     </>
