d8d990a60bd8ebfc4c9ce6da6a4be2689f3cc001
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-26 15:58:05 +0000

    udpate the page to view all tasks

--- src/features/pages/AllTasks.tsx
+++ src/features/pages/AllTasks.tsx
@@ -1,11 +1,11 @@
 import NoTasksPreview from "../components/NoTasksPreview";
 
-import TasksList from "./TaskList";
+import TasksList from "../components/TaskList";
 import { useAppDispatch, useAppSelector } from "@/lib/utils";
 import { selectTasks, setTasks } from "../redux/slices/taskSlice";
 import { GET_ALL_TASKS } from "../services/tasks";
 import { useLoader } from "@/hooks/useLoader";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 const AllTasks = () => {
   const { showLoader } = useLoader();
@@ -13,34 +13,29 @@ const AllTasks = () => {
   const dispatch = useAppDispatch();
 
   // get all tasks from services
-  const getAllTasks = async () => {
+  const getAllTasks = useCallback(async () => {
     showLoader(true);
-
     try {
       const data = await GET_ALL_TASKS();
-
       if (data) {
         dispatch(setTasks(data));
       }
     } catch (error) {
-      console.log("error: ", error);
+      console.error("Failed to fetch tasks:", error);
+      // Optionally, show an error message to the user
     } finally {
       showLoader(false);
     }
-  };
+  }, [dispatch, showLoader]);
 
   useEffect(() => {
     void getAllTasks();
   }, []);
 
   return (
-    <>
-      <section className="w-full">
-        {tasks.length === 0 && <NoTasksPreview />}
-
-        {tasks.length ? <TasksList tasks={tasks} /> : null}
-      </section>
-    </>
+    <section className="w-full">
+      {tasks.length === 0 ? <NoTasksPreview /> : <TasksList tasks={tasks} />}
+    </section>
   );
 };
 
