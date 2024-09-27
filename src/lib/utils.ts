0ab74b6745f59c136122972e6bb58d1d512c5c69
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-09-27 16:01:24 +0000

    extract the formatDate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,10 +2,15 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/features/redux/store";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDate = (date: Date) => {
+  return format(date, "EEE, dd MMM");
+};
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
