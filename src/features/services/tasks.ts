30649668b1a5d145202a3ad22d415d55d0c84a5d
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-10-09 07:29:44 +0000

    modify tasks services

--- src/features/services/tasks.ts
+++ src/features/services/tasks.ts
@@ -13,18 +13,6 @@ export const GET_TASK = async (id: number): Promise<Task> => {
   return res.data;
 };
 
-// fetch important tasks
-export const GET_IMPORTANT_TASKS = async (): Promise<Task[]> => {
-  const res = await api.get<Task[]>("/tasks/important/");
-  return res.data;
-};
-
-// fetch planned tasks
-export const GET_PLANNED_TASKS = async (): Promise<Task[]> => {
-  const res = await api.get<Task[]>("/tasks/planned/");
-  return res.data;
-};
-
 // add task
 export const ADD_TASK = async (task: AddTask): Promise<Task> => {
   const res = await api.post<Task>("/tasks/", task);
